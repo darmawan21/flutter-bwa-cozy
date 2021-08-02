6e1dc9aecb741c4ff5e9b5948c5fb1e97833ef90
darmawan21 <ddarmawan2111@gmail.com>
2021-08-02 15:22:40 +0700

    add background color in home page and splash page, change double into int, and change text in space card

--- lib/widgets/space_card.dart
+++ lib/widgets/space_card.dart
@@ -1,4 +1,5 @@
 import 'package:bwa_cozy/models/space.dart';
+import 'package:bwa_cozy/pages/detail_page.dart';
 import 'package:flutter/material.dart';
 import 'package:bwa_cozy/theme.dart';
 
@@ -9,98 +10,108 @@ class SpaceCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        ClipRRect(
-          borderRadius: BorderRadius.circular(
-            18,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailPage(),
           ),
-          child: Container(
-            width: 130,
-            height: 110,
-            child: Stack(
-              children: [
-                Image.asset(
-                  space.imageUrl,
-                ),
-                Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    width: 70,
-                    height: 30,
-                    decoration: BoxDecoration(
-                      color: purpleColor,
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(
-                          36,
+        );
+      },
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(
+              18,
+            ),
+            child: Container(
+              width: 130,
+              height: 110,
+              child: Stack(
+                children: [
+                  Image.asset(
+                    space.imageUrl,
+                  ),
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 70,
+                      height: 30,
+                      decoration: BoxDecoration(
+                        color: purpleColor,
+                        borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(
+                            36,
+                          ),
                         ),
                       ),
-                    ),
-                    child: Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image.asset(
-                            'assets/images/icon_star.png',
-                            width: 22,
-                            height: 22,
-                          ),
-                          Text(
-                            '${space.rating}/5',
-                            style: whiteTextStyle.copyWith(
-                              fontSize: 13,
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              'assets/images/icon_star.png',
+                              width: 22,
+                              height: 22,
                             ),
-                          ),
-                        ],
+                            Text(
+                              '${space.rating}/5',
+                              style: whiteTextStyle.copyWith(
+                                fontSize: 13,
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              space.name,
-              style: blackTextStyle.copyWith(
-                fontSize: 18,
-              ),
-            ),
-            SizedBox(
-              height: 2,
-            ),
-            Text.rich(
-              TextSpan(
-                text: '\$${space.price}',
-                style: purpleTextStyle.copyWith(
-                  fontSize: 16,
-                ),
-                children: [
-                  TextSpan(
-                    text: '/ month',
-                    style: greyextStyle.copyWith(
-                      fontSize: 16,
                     ),
                   ),
                 ],
               ),
             ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              '${space.city}, ${space.country}',
-              style: greyextStyle,
-            ),
-          ],
-        ),
-      ],
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                space.name,
+                style: blackTextStyle.copyWith(
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 2,
+              ),
+              Text.rich(
+                TextSpan(
+                  text: '\$${space.price}',
+                  style: purpleTextStyle.copyWith(
+                    fontSize: 16,
+                  ),
+                  children: [
+                    TextSpan(
+                      text: '/ month',
+                      style: greyextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                '${space.city}, ${space.country}',
+                style: greyextStyle,
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
